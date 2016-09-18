98ca27705aaf80d4f65c53a34760975ed29aa073
Alessandro Gatti <a.gatti@frob.it>
2016-09-19 00:02:31 +0800

    Migrated old board demos to MPLAB X.

--- Firmware-Demos/Blink_MODE_LED/main.c
+++ Firmware-Demos/Blink_MODE_LED/main.c
@@ -1,38 +1,44 @@
+#pragma config POSCMOD = NONE
+#pragma config I2C1SEL = PRI
+#pragma config IOL1WAY = ON
+#pragma config OSCIOFNC = ON
+#pragma config FCKSM = CSDCMD
+#pragma config FNOSC = FRCPLL
+#pragma config SOSCSEL = SOSC
+#pragma config WUTSEL = LEG
+#pragma config IESO = ON
+#pragma config WDTPS = PS32768
+#pragma config FWPSA = PR128
+#pragma config WINDIS = ON
+#pragma config FWDTEN = OFF
+#pragma config ICS = PGx1
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
+
+#include <xc.h>
+
 #include <p24fxxxx.h>
-
-_CONFIG1(	JTAGEN_OFF & 	//JTAG port is disabled
-			GCP_OFF & 		//GSP Memory Code Protection OFF
-			GWRP_OFF & 		//GCC Flash Write Protection OFF
-			COE_OFF & 		//
-			FWDTEN_OFF & 	//Watchdog Timer OFF
-			ICS_PGx1) 		//debug over PGD1 and PGC1
-	
-_CONFIG2(	FNOSC_FRCPLL & 	//Internal FRC with PLL
-			OSCIOFNC_ON &	//RA3 is clk out (fosc/2)
-			POSCMOD_NONE & 	//Primary oscillator disabled
-			I2C1SEL_PRI)    //Use default SCL1/SDA1 pins
-
 
 #pragma code
 
-int main(void)
-{
-   unsigned long i;         //a 32 bit variable to use as a timer
- 
-   CLKDIVbits.RCDIV0=0;     //clock divider to 0
-   AD1PCFG = 0xFFFF;        // Default all pins to digital
-   OSCCONbits.SOSCEN=0;     //Disables the secondary oscilator
+int main(void) {
+  unsigned long i; // a 32 bit variable to use as a timer
 
-   TRISAbits.TRISA1 = 0;    //sets the Mode LED pin RA1 as output
-   LATAbits.LATA1 = 0;      //turns LED off
-	
-//////////////////////////////////////////////////////////////////
-///FOREVER LOOP///////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-    while(1)
-    {
-      LATAbits.LATA1 = ~LATAbits.LATA1; //toggles the MODE LED
-      i =0xFFFFF;                       //sets i to 1048575
-      while (i--);                      //delay function
-    }                                   //decrements i until 0   
-}                                      
+  CLKDIVbits.RCDIV0 = 0; // clock divider to 0
+  AD1PCFG = 0xFFFF;      // Default all pins to digital
+  OSCCONbits.SOSCEN = 0; // Disables the secondary oscilator
+
+  TRISAbits.TRISA1 = 0; // sets the Mode LED pin RA1 as output
+  LATAbits.LATA1 = 0;   // turns LED off
+
+  //////////////////////////////////////////////////////////////////
+  /// FOREVER LOOP///////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////
+  while (1) {
+    LATAbits.LATA1 = ~LATAbits.LATA1; // toggles the MODE LED
+    i = 0xFFFFF;                      // sets i to 1048575
+    while (i--)
+      ; // delay function
+  }     // decrements i until 0
+}
--- Firmware-Demos/BusPirateADCDemo/main.c
+++ Firmware-Demos/BusPirateADCDemo/main.c
@@ -1,65 +1,74 @@
-#include <p24fxxxx.h>
+#pragma config POSCMOD = NONE
+#pragma config I2C1SEL = PRI
+#pragma config IOL1WAY = ON
+#pragma config OSCIOFNC = OFF
+#pragma config FCKSM = CSDCMD
+#pragma config FNOSC = FRCDIV
+#pragma config SOSCSEL = SOSC
+#pragma config WUTSEL = LEG
+#pragma config IESO = ON
+#pragma config WDTPS = PS32768
+#pragma config FWPSA = PR128
+#pragma config WINDIS = ON
+#pragma config FWDTEN = ON
+#pragma config ICS = PGx1
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = ON
 
-_CONFIG1(    JTAGEN_OFF &     //JTAG port is disabled
-            GCP_OFF &         //GSP Memory Code Protection OFF
-            GWRP_OFF &        //GCC Flash Write Protection OFF
-            COE_OFF &         //
-            FWDTEN_OFF &      //Watchdog Timer OFF
-            ICS_PGx1)         //debug over PGD1 and PGC1
-   
-_CONFIG2(    FNOSC_FRCPLL &   //Internal FRC with PLL
-            OSCIOFNC_ON &     //RA3 is clk out (fosc/2)
-            POSCMOD_NONE &    //Primary oscillator disabled
-            I2C1SEL_PRI)      //Use default SCL1/SDA1 pins
+#include <xc.h>
+
+#include <p24fxxxx.h>
 
 #define VREG33_DIR TRISAbits.TRISA0
 #define VREG33_EN LATAbits.LATA0
 #define MODE_LED_DIR TRISAbits.TRISA1
 #define MODE_LED LATAbits.LATA1
 
-
 #pragma code
 
-int main(void)
-{
+int main(void) {
 
-    CLKDIVbits.RCDIV0=0;     //clock divider to 0
-    AD1PCFG = 0xFFFF;        // Default all pins to digital
-    OSCCONbits.SOSCEN=0;     //Disables the secondary oscilator
-   
-    MODE_LED_DIR = 0;        //sets the Mode LED pin RA1 as output
-    MODE_LED = 0;            //turns LED off
-    VREG33_DIR =0;           //sets teh VREG pin RA0 as output
-    VREG33_EN = 1;           //turns on the voltage regulator
+  CLKDIVbits.RCDIV0 = 0; // clock divider to 0
+  AD1PCFG = 0xFFFF;      // Default all pins to digital
+  OSCCONbits.SOSCEN = 0; // Disables the secondary oscilator
 
-    unsigned long int i,voltage;      
-//////////////////////////////////////////////////////////////
-///////////////ADC config//////////////////////////////////
-    AD1PCFGbits.PCFG12=0;          //configure RB12 as analog
-    AD1CON1bits.SSRC  = 0b111;     // SSRC<3:0> = 111 implies internal
-                                   // counter ends sampling and starts
-                                   // converting.
-    AD1CON3 = 0x1F02;              // Sample time = 31Tad,
-                                   // Tad = 2 Tcy
-    AD1CHS =12;                    //ADC channel select 12
-    AD1CON1bits.ADON =1;           // turn ADC on
-///FOREVER LOOP///////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
-    while(1)
-    {
-        //this just gives us a little delay between measurements
-        i =0xFFFFF;                 //sets i to 1048575
-        while (i--);                //delay function
+  MODE_LED_DIR = 0; // sets the Mode LED pin RA1 as output
+  MODE_LED = 0;     // turns LED off
+  VREG33_DIR = 0;   // sets teh VREG pin RA0 as output
+  VREG33_EN = 1;    // turns on the voltage regulator
 
-        //start a measurement with the ADC
-        AD1CON1bits.DONE=0;         //resets DONE bit
-        AD1CON1bits.SAMP=1;         //start sample
-        
-        while(AD1CON1bits.DONE==0); //wait for conversion to finish
+  unsigned long int i, voltage;
+  //////////////////////////////////////////////////////////////
+  ///////////////ADC config//////////////////////////////////
+  AD1PCFGbits.PCFG12 = 0;   // configure RB12 as analog
+  AD1CON1bits.SSRC = 0b111; // SSRC<3:0> = 111 implies internal
+                            // counter ends sampling and starts
+                            // converting.
+  AD1CON3 = 0x1F02;         // Sample time = 31Tad,
+                            // Tad = 2 Tcy
+  AD1CHS = 12;              // ADC channel select 12
+  AD1CON1bits.ADON = 1;     // turn ADC on
+  /// FOREVER LOOP///////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////
+  while (1) {
+    // this just gives us a little delay between measurements
+    i = 0xFFFFF; // sets i to 1048575
+    while (i--)
+      ; // delay function
 
-        //get the measurement and use it to control the LED
-        voltage = ADC1BUF0;         //get the voltage measurement
-        if (voltage > 0x1D1) MODE_LED = 1; //enable LED if measurement is > 3volts
-        else MODE_LED = 0;           //disable LED if less than 3volts
-   }
+    // start a measurement with the ADC
+    AD1CON1bits.DONE = 0; // resets DONE bit
+    AD1CON1bits.SAMP = 1; // start sample
+
+    while (AD1CON1bits.DONE == 0)
+      ; // wait for conversion to finish
+
+    // get the measurement and use it to control the LED
+    voltage = ADC1BUF0; // get the voltage measurement
+    if (voltage > 0x1D1)
+      MODE_LED = 1; // enable LED if measurement is > 3volts
+    else
+      MODE_LED = 0; // disable LED if less than 3volts
+  }
 }
